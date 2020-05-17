c12e9ea9b33ffb8e712a36f09231683c66eac7eb
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-05-17 13:15:23 +0100

    counts!

--- templates/script.js
+++ templates/script.js
@@ -2,3 +2,8 @@
 
 status_node = document.getElementById('spong');
 status_node.innerHTML = 'js-done'
+
+es = new EventSource("updates");
+es.onmessage = function(event) {
+  status_node.innerHTML = event.data;
+}
