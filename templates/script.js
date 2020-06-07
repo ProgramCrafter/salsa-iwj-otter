8b895735ef8428add1b4671308bf622bbef162c5
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-06-07 20:39:02 +0100

    wip
    html5 dnd is busted for svg, cannot use it

--- templates/script.js
+++ templates/script.js
@@ -5,6 +5,20 @@
 status_node = document.getElementById('spong');
 status_node.innerHTML = 'js-done'
 
+var our_dnd_type = "text/puvnex-game-server-dummy";
+
+  console.log('foo1');
+function test_dragstart(event) {
+  console.log('foo2', event);
+  if (event.target instanceof HTMLLIElement) {
+    event.dataTransfer.setData(our_dnd_type,
+			       event.target.dataset.objid);
+    event.dataTransfer.effectAllowed = "move";
+  } else {
+    event.preventDefault();
+  }
+}
+
 es = new EventSource("updates");
 es.onmessage = function(event) {
   status_node.innerHTML = event.data;
