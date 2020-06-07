eb71cc97a6e34449c0cc45cd3845fcb22a0eed2b
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-06-07 21:19:03 +0100

    wip non dnd

--- templates/script.js
+++ templates/script.js
@@ -7,28 +7,42 @@ status_node.innerHTML = 'js-done'
 
 var our_dnd_type = "text/puvnex-game-server-dummy";
 
+dragthresh = 5;
+
   console.log('foo1');
 
 function drag_mousedown(e) {
   console.log('mousedown', e);
   dcx = e.clientX;
   dcy = e.clientY;
+  dragging = false;
   window.addEventListener('mousemove', drag_mousemove, true);
   window.addEventListener('mouseup',   drag_mouseup,   true);
 }
 
 function drag_mousemove(e) {
+  ddx = e.clientX - dcx;
+  ddy = e.clientY - dcy;
+  if (!dragging) {
+    ddr2 = ddx*ddx + ddy*ddy;
+    if (ddr2 > dragthresh) {
+      dragging = true;
+    }
+  }
   console.log('mousemove',
-	      e.clientX - dcx,
-	      e.clientY - dcy);
+	      ddx, ddy, dragging);
 }
 
 function drag_mouseup(e) {
-  console.log('mouseup',
-	      e.clientX - dcx,
-	      e.clientY - dcy);
+  console.log('mouseup');
+  drag_mousemove(e);
   window.removeEventListener('mousemove', drag_mousemove, true);
   window.removeEventListener('mouseup',   drag_mouseup,   true);
+  if (dragging) {
+    console.log('dragged', ddx, ddy);
+  } else {
+    console.log('clicked');
+  }
 }
 
 es = new EventSource("updates");
