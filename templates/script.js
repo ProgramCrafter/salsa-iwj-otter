ec083f95844c167201efefdbbf09b38f10c69f86
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-06-07 21:15:12 +0100

    wip non dnd

--- templates/script.js
+++ templates/script.js
@@ -8,15 +8,27 @@ status_node.innerHTML = 'js-done'
 var our_dnd_type = "text/puvnex-game-server-dummy";
 
   console.log('foo1');
-function test_dragstart(event) {
-  console.log('foo2', event);
-  if (event.target instanceof HTMLLIElement) {
-    event.dataTransfer.setData(our_dnd_type,
-			       event.target.dataset.objid);
-    event.dataTransfer.effectAllowed = "move";
-  } else {
-    event.preventDefault();
-  }
+
+function drag_mousedown(e) {
+  console.log('mousedown', e);
+  dcx = e.clientX;
+  dcy = e.clientY;
+  window.addEventListener('mousemove', drag_mousemove, true);
+  window.addEventListener('mouseup',   drag_mouseup,   true);
+}
+
+function drag_mousemove(e) {
+  console.log('mousemove',
+	      e.clientX - dcx,
+	      e.clientY - dcy);
+}
+
+function drag_mouseup(e) {
+  console.log('mouseup',
+	      e.clientX - dcx,
+	      e.clientY - dcy);
+  window.removeEventListener('mousemove', drag_mousemove, true);
+  window.removeEventListener('mouseup',   drag_mouseup,   true);
 }
 
 es = new EventSource("updates");
