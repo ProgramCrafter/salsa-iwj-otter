12c5b85ccb9756fe87fcdfbfdc12ef54215d07d5
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-07-07 21:25:05 +0100

    drop some now-superseded junk

--- junk/error.rs
+++
@@ -1,8 +0,0 @@
-
-#[derive(Error)]
-enum Error {
-  [error("attempt to create instance with same name as existing instance")]
-  InstanceAlreadyExists,
-  [error("attempt to create instance with same name as existing instance")]
-  UnknownInstance,
-}
