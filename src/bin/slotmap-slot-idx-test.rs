6788916cc715368b0f1c169e83495d0b00e4a6d4
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-07-18 15:01:35 +0100

    results from my serde

--- src/bin/slotmap-slot-idx-test.rs
+++ src/bin/slotmap-slot-idx-test.rs
@@ -2,12 +2,12 @@
 // results:
 // rustc 1.46.0-nightly --release
 //
-// slotmap::Keydata::get_idx_version      3.259 3.185 3.220
+// slotmap::Keydata::get_idx_version               3.259 3.185 3.220
+// slotmap_slot_idx::KeyData::get_idx_version      3.724 3.750 3.665
 
-use slotmap::KeyData;
+use game::slotmap_slot_idx;
 
 const BATCH : usize = 128;
-const ITERS : usize = 1024*1024;
 const ITERS : usize = 10*1024*1024;
 
 fn main () {
@@ -19,8 +19,8 @@ fn main () {
     let mut output = [ 0u32; BATCH ];
 
     for (&i,o) in input.iter().zip(output.iter_mut()) {
-      let kd = KeyData::from_ffi(i);
-      let (vsn,_) = kd.get_idx_version();
+      let kd = slotmap::KeyData::from_ffi(i);
+      let (vsn,_) = slotmap_slot_idx::KeyData::get_idx_version(kd);
       *o = vsn;
     }
 
