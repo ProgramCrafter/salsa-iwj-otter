0f7a3df11735af6ddadf32154b5452d4e6d8fe9e
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-07-26 01:30:05 +0100

    wip cmds

--- src/cmdlistener.rs
+++ src/cmdlistener.rs
@@ -36,16 +36,11 @@ type CSE = anyhow::Error;
 impl CommandStream<'_> {
   #[throws(CSE)]
   pub fn mainloop(mut self) {
-    loop {
-      match self.read.next() {
-        None => break,
-        Some(l) => {
-          let l = l.context("read")?;
-          decode_and_process(&mut self, &l)?;
-          write!(&mut self.write, "\n")?;
-          self.write.flush()?;
-        },
-      }
+    while let Some(l) = self.read.next() {
+      let l = l.context("read")?;
+      decode_and_process(&mut self, &l)?;
+      write!(&mut self.write, "\n")?;
+      self.write.flush()?;
     }
   }
 }
