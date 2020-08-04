d5420d7c1f8246a79b17875bbe52e73f6af29bd0
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-08-05 00:48:37 +0100

    wip clap

--- src/bin/otter.rs
+++ src/bin/otter.rs
@@ -1,7 +1,24 @@
 //
 
-fn create_table() {
-  
+use structopt::StructOpt;
+
+#[derive(Debug,StructOpt)]
+#[structopt(rename_all="kebab-case")]
+struct MainOpts {
+  #[structopt(long,group="scope",overrides_with("scope"))] server_scope: bool,
+  #[structopt(long,group="scope",overrides_with("scope"))] unix: bool,
+  #[structopt(long,group="scope",overrides_with("scope"))] unix_user: Option<String>,
+  #[structopt(subcommand)]
+  cmd: Subcommand,
 }
 
-fn main() { todo!() }
+#[derive(Debug,StructOpt)]
+enum Subcommand {
+  CreateTable {
+  }
+}
+
+fn main() {
+  let opts = MainOpts::from_args();
+  println!("{:?}", &opts);
+}
