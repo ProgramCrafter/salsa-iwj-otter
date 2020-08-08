2fb30a6c2390ab65b056601c830227f6bc9156c2
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-08-08 11:58:22 +0100

    some imports

--- src/imports.rs
+++ src/imports.rs
@@ -28,6 +28,8 @@ pub use std::os::unix;
 pub use std::time::Instant;
 pub use std::path::PathBuf;
 pub use std::os::unix::ffi::OsStrExt;
+pub use std::env;
+pub use std::process::exit;
 
 pub use thiserror::Error;
 pub use anyhow::{Context,anyhow};
