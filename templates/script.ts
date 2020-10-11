286a75219e97d5c581f33d740e05502c0ebc4318
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-10-11 23:18:33 +0100

    wip try provide own textdecoder
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- templates/script.ts
+++ templates/script.ts
@@ -1013,9 +1013,11 @@ function test_swap_stack() {
 
 function startup() {
   console.log('STARTUP');
-  let mut = wasm_bindgen.mutable('4200000000');
-  let y = mut.next();
-  console.log('FOO', mut, y);
+//  let mut = wasm_bindgen.mutable('4200000000');
+//  let y = mut.next();
+//  console.log('FOO', mut, y);
+  let x = wasm_bindgen.jsstring(12);
+  console.log(x);
 
   var body = document.getElementById("main-body")!;
   ctoken = body.dataset.ctoken!;
