403024bccbdf1410a087196fca3b7a62b273b7a9
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-10-11 22:12:35 +0100

    wip wasm bindings
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- wasm/wasm.rs
+++ wasm/wasm.rs
@@ -1,11 +1,40 @@
 
 use wasm_bindgen::prelude::*;
 use fehler::throws;
+use std::fmt::Display;
 
 use zcoord::ZCoord;
 
 #[wasm_bindgen]
 pub struct ZCoordIterator (zcoord::Mutable);
+
+trait WasmError {
+  fn e(self) -> JsValue;
+}
+
+impl<E> WasmError for E where E: Display {
+  fn e(self) -> JsValue {
+    let s = format!("{}", self);
+    JsValue::from_str(&s)
+  }
+}
+
+trait WasmResult<V> {
+  fn e(self) -> Result<V,JsValue>;
+}
+
+impl<E:Display, V> WasmResult<V> for Result<V, E> {
+  fn e(self) -> Result<V, JsValue> { self.map_err(WasmError::e) }
+}
+
+#[throws(JsValue)]
+#[wasm_bindgen]
+pub fn check(packed: &JsValue) {
+  let s = packed.as_string().ok_or_else(
+    ||"packed Z coordinate wrong JS type (not a string)"
+  ).e()?;
+  ZCoord::check_str(&s).ok_or(zcoord::ParseError).e()?;
+}
 
 const X : &'static str = "invalid value passed to wasm";
 
