19ce91659012f36be9f187ee841ee4045f45e0c8
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-11-27 23:57:08 +0000

    drop unused imports
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/imports.rs
+++ src/imports.rs
@@ -98,9 +98,6 @@ pub use delegate::delegate;
 pub use itertools::Itertools;
 pub use itertools::EitherOrBoth;
 
-pub use serde_with;
-pub use serde_with::serde_as;
-
 pub use ordered_float::OrderedFloat;
 
 pub use either::{Either,Left,Right};
