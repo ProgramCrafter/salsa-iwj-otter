f2a6cbc9ac0172be38ba0e6573278400c0b09d93
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-11-30 18:37:42 +0000

    Provide /_/README.md too
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/bin/daemon-otter.rs
+++ src/bin/daemon-otter.rs
@@ -20,6 +20,7 @@ type RL = ResourceLocation;
 
 const RESOURCES : &[(&'static str, ResourceLocation, ContentType)] = &[
   ("script.js",    RL::Main,                       ContentType::JavaScript),
+  ("README.md",    RL::Main,                       ContentType::Plain),
   ("LICENCE",      RL::Main,                       ContentType::Plain),
   ("libre",        RL::Main,                       ContentType::HTML),
   ("AGPLv3",       RL::Main,                       ContentType::Plain),
