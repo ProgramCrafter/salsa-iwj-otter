c94e1a58add7202a1c963f63235ef6c34d7325fc
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-01-10 13:31:05 +0000

    otterlib: wip new arrangements
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/bin/otterlib.rs
+++ src/bin/otterlib.rs
@@ -1,9 +1,27 @@
+// Copyright 2020 Ian Jackson
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// There is NO WARRANTY.
+
 pub use otter::imports::*;
+
+use structopt::StructOpt;
+
+#[derive(Debug,Clone)]
+#[derive(StructOpt)]
+pub struct Opts {
+  #[structopt(long="--libs", default_value="library/*.toml")]
+  libs: String,
+
+  #[structopt(long="--items", default_value="*")]
+  items: String,
+}
 
 #[throws(anyhow::Error)]
 fn main() {
-  let mut a = env::args();
-  a.next().unwrap();
+  let opts = Opts::from_args();
+  let libs = shapelib::Config1::PathGlob(opts.libs.clone());
+  shapelib::load(&vec![libs])?;
+/*
   let name = a.next().unwrap();
   let dirname = a.next().unwrap();
   let catalogue = format!("{}.toml", &dirname);
@@ -11,4 +29,5 @@ fn main() {
     name, dirname, catalogue
   };
   shapelib::load1(&e).unwrap();
+*/
 }
