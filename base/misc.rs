77c101eaeb06f0aacced34f782eb7e5d307062a0
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-04-02 13:39:40 +0100

    if_let: Slight formatting adjustment
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- base/misc.rs
+++ base/misc.rs
@@ -67,7 +67,9 @@ pub fn space_rect_attrs(table_size: PosC<f64>) -> SvgAttrs {
 
 #[macro_export]
 macro_rules! if_let {
-  { $variant:ident($binding:pat) = $input:expr; else $($otherwise:tt)* } => {
+  { $variant:ident($binding:pat) = $input:expr;
+    else $($otherwise:tt)*
+  } => {
     let $binding = match $input {
       $variant(y) => y,
       _ => { $($otherwise)* },
