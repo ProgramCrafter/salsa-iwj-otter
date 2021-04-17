0a2b04aceb31e3172b9f557590edb30dd2da49f4
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-04-17 13:45:54 +0100

    packetframe: tests: LumpReader tests short reads
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/packetframe.rs
+++ src/packetframe.rs
@@ -377,4 +377,32 @@ fn write_test(){
     expect_good(&mut rd, b"longer!");
   };
   read_all(&mut &*msg.buf);
+
+  #[derive(Debug)]
+  struct LumpReader<R: Read> {
+    inner: R,
+    inlump: usize,
+    lumpsize: usize,
+  }
+  impl<R:Read> LumpReader<R> {
+    fn new(lumpsize: usize, inner: R) -> Self {
+      LumpReader { inner, lumpsize, inlump: 0 }
+    }
+  }
+  impl<R:Read> Read for LumpReader<R> {
+    #[throws(io::Error)]
+    fn read(&mut self, buf: &mut [u8]) -> usize {
+      if self.inlump == 0 { self.inlump = self.lumpsize }
+      let want = min(self.inlump, buf.len());
+      let r = self.inner.read(&mut buf[0..want])?;
+      self.inlump -= r;
+      r
+    }
+  }
+
+  for lumpsize in 1..msg.buf.len() {
+    dbgc!(lumpsize);
+    let mut lr = LumpReader::new(lumpsize, &*msg.buf);
+    read_all(&mut lr);
+  }
 }
