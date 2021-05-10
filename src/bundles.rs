36da462250abad1db53bada2face99e046061041
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-05-10 01:21:11 +0100

    bundles: Save aux after incorporating
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/bundles.rs
+++ src/bundles.rs
@@ -569,13 +569,14 @@ impl Uploading {
 
 impl InstanceBundles {
   #[throws(MgmtError)]
-  pub fn finish_upload(&mut self, ig: &mut Instance,
+  pub fn finish_upload(&mut self, ig: &mut InstanceGuard,
                        Uploaded { id, parsed }: Uploaded) {
     let tmp = id.path_tmp(&ig.name);
     let install = id.path_(&ig.name);
 
     incorporate_bundle(self, ig, id, parsed)?;
 
+    ig.save_aux_now()?;
     self.updated(ig);
     match self.bundles.get(usize::from(id.index)) {
       Some(Some(Note { state: State::Loaded(..), .. })) => {
