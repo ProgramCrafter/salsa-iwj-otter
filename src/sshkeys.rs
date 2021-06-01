b60e35d216ac9b088f8fd91fcbc97c6195c08f30
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-06-01 21:48:45 +0100

    sshkeys: Fix formatting of ssh authorized_keys files
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/sshkeys.rs
+++ src/sshkeys.rs
@@ -449,8 +449,8 @@ impl Global {
     for (id, key) in &self.keys {
       let fp = match key.fp { Some(Ok(ref fp)) => fp, _ => continue };
       if key.refcount == 0 { continue }
-      writeln!(w, r#"{},command="{} mgmtchannel-proxy --restrict-ssh {}:{}" \
-                     {} {}:{}"#, 
+      writeln!(w,
+ r#"{},command="{} mgmtchannel-proxy --restrict-ssh {}:{}" {} {}:{}"#, 
                RESTRICTIONS,
                &config().ssh_proxy_bin, id, key.nonce,
                &key.data,
