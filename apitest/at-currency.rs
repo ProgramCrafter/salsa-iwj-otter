acfc65b4cf298a536fa275804e3344ed0b4b3032
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-01 20:36:30 +0100

    at-currency: rename bn to bn1
    These are being taken in sequence.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- apitest/at-currency.rs
+++ apitest/at-currency.rs
@@ -12,20 +12,20 @@ impl Ctx {
     let mut alice = self.connect_player(&self.alice)?;
     let mut a_pieces = alice.pieces::<PIA>()?;
 
-    let bn = a_pieces.find_by_desc_glob("*400ƒ*");
+    let bn1 = a_pieces.find_by_desc_glob("*400ƒ*");
 
     let other_pile = PosC::new(40,20);
 
-    alice.api_piece_op_single(PuSynch((&mut a_pieces, bn)).id(), (
+    alice.api_piece_op_single(PuSynch((&mut a_pieces, bn1)).id(), (
       "multigrab", json!({ "n": 50, 'z': "q000000000" })
     ))?;
     alice.synchu(&mut a_pieces)?;
 
-    let bn_desc = a_pieces[bn].info["desc"].as_str().unwrap();
+    let bn_desc = a_pieces[bn1].info["desc"].as_str().unwrap();
     assert!( bn_desc.contains("50ƒ"), "{bn_desc}" );
     let _change = a_pieces.find_by_desc_glob("*350ƒ*");
 
-    alice.api_piece(GH::Ungrab, PuSynch((&mut a_pieces, bn)), other_pile)?;
+    alice.api_piece(GH::Ungrab, PuSynch((&mut a_pieces, bn1)), other_pile)?;
     alice.synchu(&mut a_pieces)?;
 
   }
