e652c2664001c6def9f3eaa3cac523fc551a5827
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-01 21:03:27 +0100

    at-currency: More test
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- apitest/at-currency.rs
+++ apitest/at-currency.rs
@@ -15,6 +15,7 @@ impl Ctx {
     let bn1 = a_pieces.find_by_desc_glob("*400ƒ*");
 
     let other_pile = PosC::new(40,20);
+    let temp_pos = PosC::new(40,10);
 
     alice.api_piece_op_single(PuSynch((&mut a_pieces, bn1)).id(), (
       "multigrab", json!({ "n": 50, 'z': "q000000000" })
@@ -22,10 +23,19 @@ impl Ctx {
     alice.synchu(&mut a_pieces)?;
 
     a_pieces[bn1].assert_desc_contains(" 50ƒ");
-    let _change = a_pieces.find_by_desc_glob("* 350ƒ*");
+    let bn2 = a_pieces.find_by_desc_glob("* 350ƒ*");
 
     alice.api_piece(GH::Ungrab, PuSynch((&mut a_pieces, bn1)), other_pile)?;
     alice.synchu(&mut a_pieces)?;
+
+    alice.api_piece_op_single(PuSynch((&mut a_pieces, bn2)).id(), (
+      "multigrab", json!({ "n": 13, 'z': "t000000000" })
+    ))?;
+    alice.api_piece(GH::Ungrab, PuSynch((&mut a_pieces, bn2)), temp_pos)?;
+    alice.synchu(&mut a_pieces)?;
+    a_pieces[bn2].assert_desc_contains(" 13ƒ");
+
+    let _bn3 = a_pieces.find_by_desc_glob("* 337ƒ*");
 
   }
 }
